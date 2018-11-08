ddab51e27ceda87831dd33b75247c0f6f6697faa
Cameron94857 <42763015+Cameron94857@users.noreply.github.com>
2018-11-08 10:06:07 -0800

    Added JUnit tests 1.0

---
+++ app/src/test/java/ttr/up/edu/tickettoride/TTR_GameStateTest.java
@@ -0,0 +1,32 @@
+package ttr.up.edu.tickettoride;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TTR_GameStateTest {
+
+    @Test
+    public void drawFaceUp() {
+    }
+
+    @Test
+    public void drawDeck() {
+    }
+
+    @Test
+    public void drawRouteCards() {
+    }
+
+    @Test
+    public void discardRouteCard() {
+    }
+
+    @Test
+    public void setTrainDeck() {
+    }
+
+    @Test
+    public void setFaceUpTrainCards() {
+    }
+}
